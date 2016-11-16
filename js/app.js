87b610f84a923d69fb781fd21cffc7edebbcb93d
Patrick Richardson <prichardson@conversantmedia.com>
2016-11-16 13:06:00 -0600

    remove commented code and save css to local storage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.getElementById('heading').innerHTML = localStorage['title'] || 'Just Write'; // default text
 document.getElementById('content').innerHTML = localStorage['text'] || 'This text is automatically saved every second :) '; // default text
-document.getElementById('css-content').innerHTML = localStorage['csscontent'] || 'h1 { color: blue;}'; // default text
+document.getElementById('css-content').innerHTML = localStorage['css-content'] || 'h1 { color: blue;}'; // default text
 
 
 function addStyleString(str) {
@@ -14,14 +14,16 @@ function addStyleString(str) {
     } else {
         node = testNode;
     }
-    node.innerHTML = str;
+    node.innerHTML = str.replace('\r','').replace('\n','');
     document.body.appendChild(node);
 }
  setInterval(function() { // fuction that is saving the innerHTML of the div
       localStorage['title'] = document.getElementById('heading').innerHTML; // heading div
       localStorage['text'] = document.getElementById('content').innerHTML; // content div
+      localStorage['css-content'] = document.getElementById('css-content').innerHTML; // content div
+
       document.getElementById('output').innerHTML = document.getElementById('content').textContent;
-     addStyleString(document.getElementById('css-content').innerHTML);
+     addStyleString(document.getElementById('css-content').textContent);
  }, 1000);
 
 addStyleString(document.getElementById('css-content').innerHTML);
