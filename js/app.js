e23e8b19361700ac66c6d3363cfe9eb721032a89
Patrick Richardson <prichardson@conversantmedia.com>
2016-11-17 13:22:38 -0600

    refactored editor monitor

--- js/app.js
+++ js/app.js
@@ -1,43 +1,8 @@
-document.getElementById('heading').innerHTML = localStorage.name || 'Set Editor Title Here'; // default text
-document.getElementById('content').innerHTML = localStorage.html_content || '<h1>This text is automatically saved every second :) </h1>'; // default text
-document.getElementById('css-content').innerHTML = localStorage.css_content || 'h1 { color: blue;}'; // default text
-document.getElementById('js-content').innerHTML = localStorage.js_content || 'console.log("test");'; // default text
-
-
-function addStyleString() {
-    var str = document.getElementById('css-content').textContent;
-    var testNode = document.getElementById('styleentry');
-    var node;
-    if (!testNode) {
-      node = document.createElement('style');
-      node.setAttribute('id', 'styleentry');
-     
-    } else {
-        node = testNode;
-    }
-
-    node.innerHTML = str.replace(/(<([^>]+)>)/ig,"").replace(/\s+/g, "");
-    document.body.appendChild(node);
-}
-
-function addScriptString() {
-    var script = document.getElementById('js-content').textContent;
-    var baseNode = document.getElementById('scripttag');
-    if (baseNode) {
-        baseNode.outerHTML = "";
-        delete baseNode;
-    }
-    node = document.createElement('script');
-    node.setAttribute('id','scripttag');
-    node.type = 'text/javascript';
-    node.async = true;
-    
-    
-    node.onload = function(){
-        // remote script has loaded
-    };
-    node.text = script;
- document.body.appendChild(node);   
+function seedLocalStorage() {
+    document.getElementById('heading').innerHTML = localStorage.name || 'Set Editor Title Here'; // default text
+    document.getElementById('content').innerHTML = localStorage.html_content || '<h1>This text is automatically saved every second :) </h1>'; // default text
+    document.getElementById('css-content').innerHTML = localStorage.css_content || 'h1 { color: blue;}'; // default text
+    document.getElementById('js-content').innerHTML = localStorage.js_content || 'console.log("test");'; // default text
 }
 
 function init() {
@@ -46,6 +11,7 @@ function init() {
     var cssMonitor = editorMonitor();
     var jsMonitor = editorMonitor();
 
+    seedLocalStorage();
     titleMonitor.setTarget('heading', 'name');
     htmlMonitor.setTarget('content', 'html_content');
     cssMonitor.setTarget('css-content', 'css_content');
--- js/editor-monitor.js
+++ js/editor-monitor.js
@@ -1,38 +1,72 @@
 function editorMonitor() {
-    var defaultValues = defaultValues || {};;
-    var config = { attributes: true, childList: true, characterData: true };
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation){
-            if(mutation.type === 'childList'){
-                updateLocalStorage(defaultValues.target,defaultValues.title);
-            }
-        });
-    });
+    var config = config || {};
     var updateLivePreview = function() {
         document.getElementById('output').innerHTML = document.getElementById('content').textContent;
+        addStyleString();
+        addScriptString();
     };
     var updateLocalStorage = function() {
-        localStorage[defaultValues.title] = defaultValues.target.innerHTML;
+        localStorage[config.title] = config.target.innerHTML;
         updateLivePreview();
     };
+    var editorEventListener = function() {
+        var timer;
+        clearTimeout(timer);
+        timer = setTimeout(updateLocalStorage, config.timeout);
+    };
+    function addStyleString() {
+        var str = document.getElementById('css-content').textContent;
+        var testNode = document.getElementById('styleentry');
+        var node;
+        if (!testNode) {
+        node = document.createElement('style');
+        node.setAttribute('id', 'styleentry');
+        
+        } else {
+            node = testNode;
+        }
+
+        node.innerHTML = str.replace(/(<([^>]+)>)/ig,"").replace(/\s+/g, "");
+        document.body.appendChild(node);
+    }
+
+    function addScriptString() {
+        var script = document.getElementById('js-content').textContent;
+        var baseNode = document.getElementById('scripttag');
+        var node;
+        if (baseNode) {
+            baseNode.outerHTML = "";
+            delete baseNode;
+        }
+        node = document.createElement('script');
+        node.setAttribute('id','scripttag');
+        node.type = 'text/javascript';
+        node.async = true;
+        
+        
+        node.onload = function(){
+            // remote script has loaded
+        };
+        node.text = script;
+        document.body.appendChild(node);   
+    }
 
     return {
         updateLivePreview: updateLivePreview,
         setTarget: function(id, title) {
-            defaultValues = {
+            config = {
                 id: id,
                 target:  document.getElementById(id),
-                title: title
-            }
-            return defaultValues;
+                title: title,
+                timeout: 200
+            };
+            return config;
         },
         watch: function() {
-            observer.observe(defaultValues.target, config);
-            return this;
-        },
-        endWatch: function() {
-            observer.disconnect();
+            config.target.addEventListener('keydown', editorEventListener);
             return this;
         }
     };
 }
+
+
